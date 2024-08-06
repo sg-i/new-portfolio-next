51bf1184ef8957cf111000878ac76d992f39c5f6
4017 Сгадов И.В <sgadov2vanya@yandex.ru>
2024-08-06 23:55:25 +0300

    changed contact block

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -13,21 +13,21 @@ export const Contact = ({ section, content }: ContactProps) => {
     <div id="contactSection" className="flex flex-col items-center  m-auto gap-4 lg:gap-8 mb-14">
       <SectionTitle title={section} description={content.title}></SectionTitle>
       <CornerBordersDiv>
-        <ul className="grid text-xl gap-4">
+        <ul className="grid text-xl gap-4 ">
           <li className="grid grid-cols-3 gap-5 items-center">
             <div className="font-bold">Telegram:</div>
 
             <Link
               target="_blank"
-              className="col-span-2 p-2 shadow-md rounded-lg bg-white border-zinc-200 border-[1px] hover:bg-zinc-50"
+              className="font-mono col-span-2 p-2 shadow-md rounded-lg bg-white border-zinc-200 border-[1px] hover:bg-zinc-50"
               href="https://t.me/ivanquartz">
-              <div>@ivanquartz</div>
+              @ivanquartz
             </Link>
           </li>
           <li className="grid grid-cols-3  gap-5 items-center">
             <div className=" font-bold">Email:</div>
-            <div className="col-span-2 p-2 shadow-md rounded-lg bg-white border-zinc-200 border-[1px]">
-              <div>i.sgadov@yandex.ru</div>
+            <div className="font-mono col-span-2 p-2 shadow-md rounded-lg bg-white border-zinc-200 border-[1px]">
+              i.sgadov@yandex.ru
             </div>
           </li>
         </ul>
